0593db1d64c0e73050436419d1d0307ae82e8874
Laurentin <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2024-11-28 16:14:22 +0100

    tournois fini

--- Parentselection.py
+++ Parentselection.py
@@ -6,13 +6,20 @@ N_population = 1000
 N_parents = int(0.1 * N_population)
 N_enfants = int(0.4 * N_population)
 
-def Parentcontest (Population,strategie) :
+def parentcontest (parents,strategie) :
     #Dans parents chaque individu est représenté par 21 floats dont le dernier est la valeur de fitness
-    Parents = Population[:N_parents]
-    enfants = np.array([[0.] * 21] * N_enfants)
-    if strategie =='Contest':
-        papas = np.random.choice(Parents,10)
-        
+    
+    if strategie =='tournament':
+        N_tournament =5
+        dads =[]
+        moms =[]
+        for i in range(N_enfants):
+            t = np.random.choice(parents,N_tournament*2)
+            dad = t[np.argmax(t[:N_tournament,-1])]
+            mom = t[np.argmax(t[N_tournament:,-1])]
+            dads.append(dad)
+            moms.append(mom)
+        return (np.array(moms),np.array(dads))
 
-    return (enfants)
+    
 
